5c894af3d959bde5ef6ac854ee8d3db0e8905f8f
tugce <tugceozdemiir@hotmail.com>
2023-03-08 22:39:03 +0300

    tugce

--- frontend/components/IletisimFormu.js
+++ frontend/components/IletisimFormu.js
@@ -1,18 +1,18 @@
 import React, { useState } from "react";
-import Goruntule from './Goruntule';
+import Goruntule from "./Goruntule";
 
 const formData = {
   ad: "",
   soyad: "",
   email: "",
-  mesaj: ""
+  mesaj: "",
 };
 
 const errorData = {
   ad: "",
   soyad: "",
   email: "",
-  mesaj: ""
+  mesaj: "",
 };
 
 const IletisimFormu = () => {
@@ -34,20 +34,22 @@ const IletisimFormu = () => {
     return "";
   };
 
-
   const handleSubmit = (e) => {
     e.preventDefault();
 
     const submitErrors = {};
-    Object.keys(errors).forEach(field => {
+    Object.keys(errors).forEach((field) => {
       submitErrors[field] = errorHandling(field, form[field]);
     });
 
     setErrors(submitErrors);
 
-    const hasErrors = (submitErrors.ad === "" && submitErrors.soyad === "" && submitErrors.email === "" && submitErrors.mesaj === "");
+    const hasErrors =
+      submitErrors.ad === "" &&
+      submitErrors.soyad === "" &&
+      submitErrors.email === "" &&
+      submitErrors.mesaj === "";
     setDisplayData(hasErrors);
-
   };
 
   const handleChange = (e) => {
@@ -59,71 +61,76 @@ const IletisimFormu = () => {
 
     setErrors({
       ...errors,
-      [e.target.name]: errorMessage
+      [e.target.name]: errorMessage,
     });
 
     setForm({
       ...form,
-      [e.target.name]: e.target.value
+      [e.target.name]: e.target.value,
     });
   };
 
   return (
     <div className="App">
-      <h1 data-testId="form-title">İletişim Formu</h1>
+      <h1 data-testid="form-title">İletişim Formu</h1>
       <form onSubmit={handleSubmit}>
         <div>
           <label htmlFor="ad">Ad*</label>
           <input
-            data-testid="isim"
             onChange={handleChange}
             name="ad"
             value={form.ad}
             id="ad"
             placeholder="İlhan"
+            data-testid="isim-input"
           />
-          {(errors.ad) && <p data-testid="error">Hata: {errors.ad}</p>}
+          {errors.ad && <p data-testid="error-isim">Hata: {errors.ad}</p>}
         </div>
 
         <div>
           <label htmlFor="soyad">Soyad*</label>
           <input
-          data-testid="soyad"
             onChange={handleChange}
             id="soyad"
             name="soyad"
             value={form.soyad}
             placeholder="Mansız"
+            data-testid="soyad-input"
           />
-          {(errors.soyad) && <p data-testid="error-soyad">Hata: {errors.soyad}</p>}
+          {errors.soyad && (
+            <p data-testid="error-soyad">Hata: {errors.soyad}</p>
+          )}
         </div>
 
         <div>
           <label htmlFor="email">Email*</label>
           <input
-           data-testid="mail"
             onChange={handleChange}
             id="email"
             name="email"
             value={form.email}
             placeholder="yüzyılıngolcüsü@hotmail.com"
+            data-testid="email-input"
           />
-          {(errors.email) && <p data-testid="error-mail">Hata: {errors.email}</p>}
+          {errors.email && (
+            <p data-testid="error-email">Hata: {errors.email}</p>
+          )}
         </div>
 
         <div>
           <label htmlFor="mesaj">Mesaj</label>
           <input
-          data-testid="message"
             onChange={handleChange}
             name="mesaj"
             id="mesaj"
             value={form.mesaj}
           />
-          {(errors.mesaj) && <p data-testid="error-mesaj">Error: {errors.mesaj}</p>}
+          {errors.mesaj && (
+            <p data-testid="error-mesaj">Error: {errors.mesaj}</p>
+          )}
         </div>
 
-        {displayData && <Goruntule form={form}/>}
+        {displayData && <Goruntule form={form} />}
 
         <button data-testid="submit-button">Gönder</button>
       </form>
--- frontend/components/IletisimFormu.test.js
+++ frontend/components/IletisimFormu.test.js
@@ -1,141 +1,139 @@
-import React from 'react';
-import { fireEvent, render, screen, waitFor } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
-import userEvent from '@testing-library/user-event';
-import IletisimFormu from './IletisimFormu';
+import React from "react";
+import {
+  fireEvent,
+  getByText,
+  render,
+  screen,
+  waitFor,
+} from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import userEvent from "@testing-library/user-event";
+import IletisimFormu from "./IletisimFormu";
 
-test('hata olmadan render ediliyor', () => {
-    render(<IletisimFormu/>)
+test("hata olmadan render ediliyor", () => {
+  render(<IletisimFormu />);
 });
 
-test('iletişim formu headerı render ediliyor', () => {
-    render(<IletisimFormu/>)
-    const title=screen.getByTestId("form-title");
-    expect(title).toBeInTheDocument();
-    });
-    
+test("iletişim formu headerı render ediliyor", () => {
+  render(<IletisimFormu />);
+  expect(screen.getByRole("heading")).toHaveTextContent(/İletişim Formu/);
 
-test('kullanici adini 5 karakterden az girdiğinde bir hata mesaji render ediyor.',
- async () => {
-    render(<IletisimFormu/>)
-    const isimAlani=screen.getByTestId("isim");
-    fireEvent.change(isimAlani,{target:{ value:'hey'}})
-   
-    const isimHatasi=screen.getByTestId("error");
-    expect(isimHatasi).toBeInTheDocument();
+  // const title = screen.getByTestId("form-title");
+  //expect(title).toBeInTheDocument()
 });
 
+test("kullanıcı adını 5 karakterden az girdiğinde BİR hata mesajı render ediyor.", async () => {
+  render(<IletisimFormu />);
+  const nameInput = screen.getByTestId("isim-input");
+  fireEvent.change(nameInput, { target: { value: "aaa" } });
+  expect(nameInput.value).toBe("aaa");
 
-
-test('kullanici inputlari doldurmadiginda üc hata mesaji render ediliyor.', async () => {
-    render(<IletisimFormu/>) 
-    const submitButton=screen.getByTestId("submit-button")
-    fireEvent.click(submitButton);
-    
-    const isimHatasi=screen.getByTestId("error");
-    const soyadHatasi=screen.getByTestId("error-soyad");
-    const mailHatasi=screen.getByTestId("error-mail"); 
-
-    
-    expect(isimHatasi).toBeInTheDocument();
-    expect(soyadHatasi).toBeInTheDocument();
-    expect(mailHatasi).toBeInTheDocument();
+  const nameError = screen.getByTestId("error-isim");
+  expect(nameError).toBeInTheDocument();
 });
 
+test("kullanıcı inputları doldurmadığında ÜÇ hata mesajı render ediliyor.", async () => {
+  render(<IletisimFormu />);
+  const submitButton = screen.getByTestId("submit-button");
+  fireEvent.click(submitButton);
 
-test('kullanıcı doğru ad ve soyad girdiğinde ama email girmediğinde BİR hata mesaji render ediliyor.', async () => {
-    render(<IletisimFormu/>) 
-    const isimAlani=screen.getByTestId("isim");
-    fireEvent.change(isimAlani,{target:{ value:'tugce'}})
+  const nameError = screen.getByTestId("error-isim");
+  expect(nameError).toBeInTheDocument();
 
-    const soyadAlani=screen.getByTestId("soyad");
-    fireEvent.change(soyadAlani,{target:{ value:'ozdemir'}})
+  const lastnameError = screen.getByTestId("error-soyad");
+  expect(lastnameError).toBeInTheDocument();
 
+  const emailError = screen.getByTestId("error-email");
+  expect(emailError).toBeInTheDocument();
+});
 
-    const submitButton=screen.getByTestId("submit-button")
-    fireEvent.click(submitButton);
+test("kullanıcı doğru ad ve soyad girdiğinde ama email girmediğinde BİR hata mesajı render ediliyor.", async () => {
+  render(<IletisimFormu />);
+  const nameInput = screen.getByTestId("isim-input");
+  fireEvent.change(nameInput, { target: { value: "Furkan" } });
+  expect(nameInput.value).toBe("Furkan");
 
-    const mailHatasi=screen.getByTestId("error-mail"); 
+  const lastnameInput = screen.getByTestId("soyad-input");
+  fireEvent.change(lastnameInput, { target: { value: "İslek" } });
+  expect(lastnameInput.value).toBe("İslek");
 
+  const submitButton = screen.getByTestId("submit-button");
+  fireEvent.click(submitButton);
+
+  const emailError = screen.getByTestId("error-email");
+  expect(emailError).toBeInTheDocument();
 });
 
 test('geçersiz bir mail girildiğinde "email geçerli bir email adresi olmalıdır." hata mesajı render ediliyor', async () => {
-  
-    render(<IletisimFormu/>) 
-    const mailAlani=screen.getByTestId("mail");
-    fireEvent.change(mailAlani,{target:{ value:'tugceozdemiir.com'}})
-    expect(mailAlani.value).toBe("tugceozdemiir.com");
+  render(<IletisimFormu />);
+  const emailInput = screen.getByTestId("email-input");
+  fireEvent.change(emailInput, { target: { value: "testtester.com" } });
+  expect(emailInput.value).toBe("testtester.com");
 
-    const mailHatasi=screen.getByTestId("error-mail"); 
-    expect(mailHatasi).toBeInTheDocument();
+  const emailError = screen.getByTestId("error-email");
+  expect(emailError).toBeInTheDocument();
 });
 
 test('soyad girilmeden gönderilirse "soyad gereklidir." mesajı render ediliyor', async () => {
-    
-    render(<IletisimFormu/>) 
-   
-    const isimAlani=screen.getByTestId("isim");
-    fireEvent.change(isimAlani,{target:{ value:'tugce'}})
-    expect(isimAlani).toBeInTheDocument('tugce');
+  render(<IletisimFormu />);
 
+  const nameInput = screen.getByTestId("isim-input");
+  fireEvent.change(nameInput, { target: { value: "Furki" } });
+  expect(nameInput.value).toBe("Furki");
 
-    const mailAlani=screen.getByTestId("mail");
-    fireEvent.change(mailAlani,{target:{ value:'tugceozdemiir.com'}})
-    expect(mailAlani.value).toBe("tugceozdemiir.com");
+  const emailInput = screen.getByTestId("email-input");
+  fireEvent.change(emailInput, { target: { value: "testtester.com" } });
+  expect(emailInput.value).toBe("testtester.com");
 
-    const submitButton=screen.getByTestId("submit-button")
-    fireEvent.click(submitButton);
+  const submitButton = screen.getByTestId("submit-button");
+  fireEvent.click(submitButton);
 
-    
-    const soyadHatasi=screen.getByTestId("error-soyad");
-    expect(soyadHatasi).toBeInTheDocument();
+  const emailError = screen.getByTestId("error-email");
+  expect(emailError).toBeInTheDocument();
 });
 
-test('ad,soyad, email render ediliyor. mesaj bölümü doldurulmadığında hata mesajı render edilmiyor.', async () => {
-   
-    render(<IletisimFormu/>)
-    
-    const isimAlani=screen.getByTestId("isim");
-    fireEvent.change(isimAlani,{target:{ value:'tugce'}})
-    expect(isimAlani).toBeInTheDocument('tugce');
+test("ad,soyad, email render ediliyor. mesaj bölümü doldurulmadığında hata mesajı render edilmiyor.", async () => {
+  render(<IletisimFormu />);
 
-    const soyadAlani=screen.getByTestId("soyad");
-    fireEvent.change(soyadAlani,{target:{ value:'ozdemir'}})
-    expect(soyadAlani).toBeInTheDocument('ozdemir');
+  const nameInput = screen.getByTestId("isim-input");
+  fireEvent.change(nameInput, { target: { value: "Furuk" } });
+  expect(nameInput.value).toBe("Furuk");
 
-    const mailAlani=screen.getByTestId("mail");
-    fireEvent.change(mailAlani,{target:{ value:'tugceozdemiir.com'}})
-    expect(mailAlani.value).toBe("tugceozdemiir.com");
+  const lastnameInput = screen.getByTestId("soyad-input");
+  fireEvent.change(lastnameInput, { target: { value: "İslek" } });
+  expect(lastnameInput.value).toBe("İslek");
 
-    const submitButton=screen.getByTestId("submit-button")
-    fireEvent.click(submitButton);
+  const emailInput = screen.getByTestId("email-input");
+  fireEvent.change(emailInput, { target: { value: "test@tester.com" } });
+  expect(emailInput.value).toBe("test@tester.com");
 
-    const mesajHatasi=screen.queryByTestId("error-mesaj");
-    expect(mesajHatasi).not.toBeInTheDocument();
+  const submitButton = screen.getByTestId("submit-button");
+  fireEvent.click(submitButton);
 
-
-
+  const emailError = screen.queryByTestId("error-email");
+  expect(emailError).not.toBeInTheDocument();
 });
 
-test('form gönderildiğinde girilen tüm değerler render ediliyor.', async () => {
-    render(<IletisimFormu/>)
+test("form gönderildiğinde girilen tüm değerler render ediliyor.", async () => {
+  render(<IletisimFormu />);
 
-    const isimAlani=screen.getByTestId("isim");
-    fireEvent.change(isimAlani,{target:{ value:'tugce'}})
-    expect(isimAlani).toBeInTheDocument('tugce');
+  const nameInput = screen.getByTestId("isim-input");
+  fireEvent.change(nameInput, { target: { value: "Furkan" } });
+  expect(nameInput.value).toBe("Furkan");
 
-    const soyadAlani=screen.getByTestId("soyad");
-    fireEvent.change(soyadAlani,{target:{ value:'ozdemir'}})
-    expect(soyadAlani).toBeInTheDocument('ozdemir');
+  const lastnameInput = screen.getByTestId("soyad-input");
+  fireEvent.change(lastnameInput, { target: { value: "İŞLEK" } });
+  expect(lastnameInput.value).toBe("İŞLEK");
 
-    const mailAlani=screen.getByTestId("mail");
-    fireEvent.change(mailAlani,{target:{ value:'tugceozdemiir.com'}})
-  expect(mailAlani.value).toBe("tugceozdemiir.com");
+  const emailInput = screen.getByTestId("email-input");
+  fireEvent.change(emailInput, { target: { value: "test@tester.com" } });
+  expect(emailInput.value).toBe("test@tester.com");
 
-    const submitButton=screen.getByTestId("submit-button")
-    fireEvent.click(submitButton);
+  const submitButton = screen.getByTestId("submit-button");
+  fireEvent.click(submitButton);
 
-   const displayer=screen.getByTestId("sent-data");
-    expect(displayer).toBeInTheDocument();
+  const displayer = screen.getByTestId("sent-data");
+  expect(displayer).toBeInTheDocument();
+
 
 });
